2e5325c34fd5c01d0a3852e59f381900a0c35add
constrict0r <constrict0r@protonmail.com>
2019-03-12 12:02:08 -0600

    Add simplest pybomb example

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# python-bomb
+# pyboom
 
 Simple python module to detone a bomb.
+
+# Usage:
+
+git clone https://gitlab.com/geek.log/pyboom.git
+cd pyboom
+pip install .
+python
+
+```
+# On python console.
+>>> import pyboom
+>>> print (pyboom.detonate())
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyboom',
+      version='0.1.0',
+      description='Bomb detonator .. boom!!',
+      url='https://gitlab.com/geek.log/pyboom',
+      author='constrict0r',
+      author_email='constrict0r@protonmail.com',
+      license='MIT',
+      packages=['pyboom'],
+      zip_safe=False)
