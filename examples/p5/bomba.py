6bbbe0caeb7029eb51fb48b1c3f743d5a8b51799
constrict0r <constrict0r@protonmail.com>
2019-03-16 15:40:59 -0600

    Renamed script

---
+++ examples/p5/bomba.py
@@ -0,0 +1,11 @@
+# Boom!!!
+def conteo(segundos=10):
+    if segundos < 1:
+        print('No puedo contar menos de un segundo.')
+    else:
+        for segundo_actual in reversed(range(1, segundos + 1)):
+            print(segundo_actual)
+        
+        print('BOOM!!!')
+
+conteo()
--- examples/p5/cuenta_regresiva.py
+++
@@ -1,9 +0,0 @@
-# Boom!!!
-def conteo(segundos=10):
-    if segundos < 1:
-        print('No puedo contar menos de un segundo.')
-    else:
-        for segundo_actual in reversed(range(0, segundos)):
-            print(segundo_actual + 1)
-
-conteo()
