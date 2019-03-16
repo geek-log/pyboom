7bc7df3960d1455f50cee64d3b154c42694bd2ab
constrict0r <constrict0r@protonmail.com>
2019-03-16 09:48:28 -0600

    Added part 4 examples

---
+++ examples/p4/cuenta_regresiva.py
@@ -0,0 +1,9 @@
+# Boom!!!
+def conteo(segundos=10):
+    if segundos < 1:
+        print('No puedo contar menos de un segundo.')
+    else:
+        print('Contando ', segundos, ' segundos ...')
+
+conteo(0)
+conteo(15)
