64cfe875e2f7fa243c1d44029a0f61c7a2bfa002
constrict0r <constrict0r@protonmail.com>
2020-04-20 11:35:42 -0600

    Set same version number on init and toml

--- p16/pyboom/pyboom/__init__.py
+++ p16/pyboom/pyboom/__init__.py
@@ -3,6 +3,6 @@
 """pyboom - Detonador de Bombas .. Boom!!!"""
 
 __title__ = 'pyboom'
-__version__ = '0.0.1'
+__version__ = '0.3.0'
 __author__ = 'constrict0r <constrict0r@protonmail.com>'
 __all__ = []
